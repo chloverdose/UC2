c670ffd0ec7113f6b4debf9fdb18024e5b0dc291
Mika Eriksen <mika.eriksen@hotmail.com>
2018-11-24 15:37:57 +0100

    commit

---
+++ UitslagControle/Services/SQL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UitslagControle.Services
+{
+    class SQL
+    {
+        void GetConnection()
+        {
+            MySqlConnection conn = new MySqlConnection();
+        }
+    }
+}
